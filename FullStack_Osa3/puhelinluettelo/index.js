996135afaa66f3390c347a275b5384ecd647fba5
esakemp <esa.kemppainen@helsinki.fi>
2019-02-08 17:26:20 +0200

    fix 3.9

--- FullStack_Osa3/puhelinluettelo/index.js
+++ FullStack_Osa3/puhelinluettelo/index.js
@@ -107,7 +107,7 @@ app.post('/api/persons', (req, res) => {
 
 })
 
-const PORT = process.env || 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server runnning on port ${PORT}`)
 })
